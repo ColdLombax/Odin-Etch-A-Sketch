75a679aa052fdaa966b412291ceec34cb453bcac
coldlombax <coldlombax@gmail.com>
2021-06-07 12:17:17 +0100

    Changing all the code structure with new functions

--- app.js
+++ app.js
@@ -1,58 +1,35 @@
-const gridContainer = document.querySelector('#grid-container');
+const gridContainer = document.querySelector("#grid-container");
 
-let toggleOption = 'mouseover';
-let colorOption = 'default'
-let color = 'black';
+// CUSTOM VARIABLES
+const borderOnly = false;
+const activeColor = "greyscale";
 
-// userGridSize = prompt("Pick a size between 0 - 64")
-// createGrid(userGridSize);
+const createGrids = (gridSize) => {
+  gridSize > 64 ? (gridSize = 64) : (gridSize = gridSize);
+  const gridArea = gridSize * gridSize;
 
-createGrid(10);
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-function createGrid(gridSize) {
-    gridSize > 64 ? gridSize = 64 : gridSize = gridSize;
-    const gridArea = gridSize * gridSize;
-    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-    for (i=0; i <= gridArea; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.addEventListener(toggleOption, colorPainter);
-        gridContainer.appendChild(gridItem);
+  for (i = 0; i <= gridArea - 1; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    if (!borderOnly) {
+      gridItem.classList.add("grid-item-border");
     }
-}
+    // gridItem.addEventListener(toggleOption, colorPainter);
+    gridContainer.appendChild(gridItem);
+  }
+};
 
-function colorSelector(color) {
-    colorOption = color;
-}
+createGrids(32);
 
-function colorPainter(item) {
-    switch (colorOption)
-    {
-        case 'rgb':
-            const r = Math.floor(Math.random() * 256);
-            const g = Math.floor(Math.random() * 256);
-            const b = Math.floor(Math.random() * 256);
-            item.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-            break;
-        case 'default':
-            item.target.style.backgroundColor = 'rgb(0, 0, 0)';
-            break;
-        case 'eraser':
-            item.target.style.backgroundColor = 'rgb(255, 255, 255)';
-            break;
-    }
-    
-} 
+// Toggle Options
 
-function clearGrid() {
-    gridItems = document.querySelectorAll('.grid-item');
-    for(i=0; i<gridItems.length; i++) {
-        gridItems[i].style.backgroundColor = '#fff'
-    }
-}
+let isModalOpen = false;
 
-// when clicking the hover button the inner text changes to click
-// the toggleOption is set to click
-// when changing colors and events you must foreach indivdual option
-//  change the event and color options somehow?
+const toggleModal = () => {
+  isModalOpen = !isModalOpen;
+  const modal = document.querySelector("#modal");
+  isModalOpen ? (modal.style.display = "flex") : (modal.style.display = "none");
+};
